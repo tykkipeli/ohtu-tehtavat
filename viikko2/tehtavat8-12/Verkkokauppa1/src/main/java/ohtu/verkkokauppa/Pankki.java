cf029b7318cb5e8cbcadc7256d7222a5844e0bd9
Olli Matilainen <olli.e.matilainen@gmail.com>
2019-11-11 00:49:26 +0200

    viikko2 tehtävät 8-12 tehty

--- viikko2/tehtavat8-12/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
+++ viikko2/tehtavat8-12/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -1,22 +1,19 @@
 package ohtu.verkkokauppa;
 
-public class Pankki {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-    private static Pankki instanssi;
+@Component
+public class Pankki implements PankkiRajapinta{
+    
+    private KirjanpitoRajapinta kirjanpito;
 
-    public static Pankki getInstance() {
-        if (instanssi == null) {
-            instanssi = new Pankki();
-        }
-
-        return instanssi;
-    }
-    private Kirjanpito kirjanpito;
-
-    public Pankki() {
-        kirjanpito = Kirjanpito.getInstance();
+    @Autowired
+    public Pankki(KirjanpitoRajapinta kirjanpito) {
+        this.kirjanpito = kirjanpito;
     }
 
+    @Override
     public boolean tilisiirto(String nimi, int viitenumero, String tililta, String tilille, int summa) {
         kirjanpito.lisaaTapahtuma("tilisiirto: tililtä " + tilille + " tilille " + tilille
                 + " viite " + viitenumero + " summa " + summa + "e");
